c3e36bb0030720cef0cbbc9878c9f65f9a7e7a2e
tim <tjhickey@brandeis.edu>
2022-02-09 11:58:39 -0500

    added pa01 to folder

---
+++ pa01/course_search.py
@@ -0,0 +1,69 @@
+'''
+course_search is a Python script using a terminal based menu to help
+students search for courses they might want to take at Brandeis
+'''
+
+from schedule import Schedule
+import sys
+
+schedule = Schedule()
+schedule.load_courses()
+schedule = schedule.enrolled(range(5,1000)) # eliminate courses with no students
+
+TOP_LEVEL_MENU = '''
+quit
+reset
+term  (filter by term)
+course (filter by coursenum, e.g. COSI 103a)
+instructor (filter by instructor)
+subject (filter by subject, e.g. COSI, or LALS)
+title  (filter by phrase in title)
+description (filter by phrase in description)
+timeofday (filter by day and time, e.g. meets at 11 on Wed)
+'''
+
+terms = {c['term'] for c in schedule.courses}
+
+def topmenu():
+    '''
+    topmenu is the top level loop of the course search app
+    '''
+    global schedule
+    while True:         
+        command = input(">> (h for help) ")
+        if command=='quit':
+            return
+        elif command in ['h','help']:
+            print(TOP_LEVEL_MENU)
+            print('-'*40+'\n\n')
+            continue
+        elif command in ['r','reset']:
+            schedule.load_courses()
+            schedule = schedule.enrolled(range(5,1000))
+            continue
+        elif command in ['t', 'term']:
+            term = input("enter a term:"+str(terms)+":")
+            schedule = schedule.term([term]).sort('subject')
+        elif command in ['s','subject']:
+            subject = input("enter a subject:")
+            schedule = schedule.subject([subject])
+        else:
+            print('command',command,'is not supported')
+            continue
+
+        print("courses has",len(schedule.courses),'elements',end="\n\n")
+        print('here are the first 10')
+        for course in schedule.courses[:10]:
+            print_course(course)
+        print('\n'*3)
+
+def print_course(course):
+    '''
+    print_course prints a brief description of the course 
+    '''
+    print(course['subject'],course['coursenum'],course['section'],
+          course['name'],course['term'],course['instructor'])
+
+if __name__ == '__main__':
+    topmenu()
+
